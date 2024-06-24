f2beb1895a4c966cd183cd52a72de3a0f1f5d8c7
erickshayo <erickshayo306@gmail.com>
2024-06-24 23:12:01 +0300

    create a channel

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -6,17 +6,17 @@
 # rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
-cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
+# cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
 
 
 
-# # System channel
-# SYS_CHANNEL="sys-channel"
+# System channel
+SYS_CHANNEL="sys-channel"
 
-# # channel name defaults to "mychannel"
-# CHANNEL_NAME="mychannel"
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
 
-# echo $CHANNEL_NAME
+echo $CHANNEL_NAME
 
 # # Generate System Genesis block
 # configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
@@ -25,8 +25,8 @@ cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
 # # Generate channel configuration block
 # configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
 
-# echo "#######    Generating anchor peer update for Org1MSP  ##########"
-# configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 
-# echo "#######    Generating anchor peer update for Org2MSP  ##########"
-# configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
